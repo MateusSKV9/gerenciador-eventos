bce837317ca5650d55fd9db584c3ca177c4cbb53
Mateus Santos <mateussilvanzg@gmail.com>
2026-01-18 13:51:14 -0300

    feat: improve event card

--- src/features/events/components/Event/Event.jsx
+++ src/features/events/components/Event/Event.jsx
@@ -23,11 +23,21 @@ export function Event({ name, description, date, duration }) {
 						<span className={styles.date}>{date}</span>
 						<span className={styles.duration}>{duration} Dias</span>
 					</div>
-					<select className={styles.durationType} name="durationType" id="durationType">
-						<option value="dias">Dias</option>
-					</select>
+
+					<div className={styles.container_select}>
+						<select className={styles.durationType} name="durationType" id="durationType">
+							<option value="dias">Dias</option>
+						</select>
+
+						<svg className={styles.select_icon} xmlns="http://www.w3.org/2000/svg" viewBox="0 0 640 640">
+							<path
+								fill="currentColor"
+								d="M297.4 438.6C309.9 451.1 330.2 451.1 342.7 438.6L502.7 278.6C515.2 266.1 515.2 245.8 502.7 233.3C490.2 220.8 469.9 220.8 457.4 233.3L320 370.7L182.6 233.4C170.1 220.9 149.8 220.9 137.3 233.4C124.8 245.9 124.8 266.2 137.3 278.7L297.3 438.7z"
+							/>
+						</svg>
+					</div>
 				</div>
-				<progress className={styles.progress} />
+				<progress value={30} min="00" max="100" className={styles.progress} />
 			</div>
 		</article>
 	);
