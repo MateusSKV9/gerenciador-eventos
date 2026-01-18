e56935e67d6a70331d37d13b6c175138195abb00
Mateus Santos <mateussilvanzg@gmail.com>
2026-01-18 13:51:40 -0300

    feat: implement portal

--- src/pages/Events/Events.jsx
+++ src/pages/Events/Events.jsx
@@ -1,6 +1,8 @@
+import { useState } from "react";
 import { Event } from "../../features/events/components/Event/Event";
 import { Button } from "../../shared/components/Button/Button";
 import styles from "./Events.module.css";
+import { CreateEventModal } from "../../features/events/components/CreateEventModal/CreateEventModal";
 
 const events = [
 	{ id: "1", name: "Evento1", categoryId: "1", description: "blabla", data: "30/12/2030", duration: 30 },
@@ -9,6 +11,11 @@ const events = [
 ];
 
 export function Events() {
+	const [isCreateModalOpen, setIsCreateModalOpen] = useState(false);
+
+	const showModal = () => setIsCreateModalOpen(true);
+	const closeModal = () => setIsCreateModalOpen(false);
+
 	return (
 		<section>
 			<header className={styles.header}>
@@ -33,6 +40,8 @@ export function Events() {
 					/>
 				))}
 			</div>
+
+			{isCreateModalOpen && <CreateEventModal close={closeModal} />}
 		</section>
 	);
 }
