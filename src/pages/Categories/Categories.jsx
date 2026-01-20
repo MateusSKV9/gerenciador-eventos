bea6f9796d761fbe8e21b94772916e604bbfa6fe
Mateus Santos <mateussilvanzg@gmail.com>
2026-01-20 16:24:37 -0300

    feat: add categories listing

--- src/pages/Categories/Categories.jsx
+++ src/pages/Categories/Categories.jsx
@@ -1,7 +1,23 @@
+import { CategoryCard } from "../../features/categories/components/CategoryCard/CategoryCard";
+import { useCategories } from "../../hooks/useCategories";
+import { Button } from "../../shared/components/Button/Button";
+import { SectionHeader } from "../../shared/components/SectionHeader/SectionHeader";
+import styles from "./Categories.module.css";
+
 export function Categories() {
+	const { categories } = useCategories();
+
 	return (
 		<section>
-			<h1>Categorias</h1>
+			<SectionHeader title="Categorias">
+				<Button>Nova categoria</Button>
+			</SectionHeader>
+
+			<div className={styles.container_categories}>
+				{categories.map((category) => (
+					<CategoryCard key={category.id} name={category.name} />
+				))}
+			</div>
 		</section>
 	);
 }
